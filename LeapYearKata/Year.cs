6c1886f9d32fb6a71a246ca354e94825910174bb
Raúl Corchero <rcorchero@aidacanarias.com>
2023-07-04 10:08:06 +0100

    Refactros: refactor IsLeap method

--- LeapYearKata/Year.cs
+++ LeapYearKata/Year.cs
@@ -4,9 +4,7 @@ public class Year
 {
     public static bool IsLeap(int year)
     {
-        var result = false;
-        if (year % 400 == 0 || year % 4 == 0) result = true;
-        if (year % 100 == 0 && year % 400 != 0) result =  false;
+        var result = (year % 4 == 0 &&  year % 100 != 0) || year % 400 == 0;
         return result;
     }
 }
