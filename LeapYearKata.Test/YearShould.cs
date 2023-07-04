4462ea490d63882790924efa38923e4925929d54
Raúl Corchero <rcorchero@aidacanarias.com>
2023-07-04 08:23:49 +0100

    Green: Return_true_with_year_800

--- LeapYearKata.Test/YearShould.cs
+++ LeapYearKata.Test/YearShould.cs
@@ -13,5 +13,12 @@ namespace LeapYearKata.Test
             var result = Year.IsLeap(400);
             Assert.That(result, Is.EqualTo(true));
         }
+
+        [Test]
+        public void Return_true_with_year_800()
+        {
+            var result = Year.IsLeap(800);
+            Assert.That(result, Is.EqualTo(true));
+        }
     }
 }
