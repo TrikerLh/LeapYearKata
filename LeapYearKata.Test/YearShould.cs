483d66f476cb283a44b2b72a98408c48d5808dbe
Raúl Corchero <rcorchero@aidacanarias.com>
2023-07-04 09:27:44 +0100

    Refactor: delete tests

--- LeapYearKata.Test/YearShould.cs
+++ LeapYearKata.Test/YearShould.cs
@@ -16,27 +16,13 @@ namespace LeapYearKata.Test
             Assert.That(result, Is.EqualTo(true));
         }
 
-        [Test]
-        public void Return_false_year_100()
-        {
-            var result = Year.IsLeap(100);
-            Assert.That(result, Is.EqualTo(false));
-        }
-
-        [Test]
-        public void Return_false_year_200()
-        {
-            var result = Year.IsLeap(200);
-            Assert.That(result, Is.EqualTo(false));
-        }
-
         [TestCase(100)]
         [TestCase(200)]
         [TestCase(300)]
         public void Return_false_with_year_divisible_100_but_not_divisible_400(int input)
         {
             var result = Year.IsLeap(input);
-            Assert.AreEqual(false, result);
+            Assert.That(result, Is.EqualTo(false));
         }
     }
 }
